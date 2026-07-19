fb5fb333e004edcbea355b748a2afc2230e72feb
chetan <156156147+chetan3622@users.noreply.github.com>
2026-07-19 18:32:58 +0530

    Refactor dashboard comment in app.py

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def logout():
     flash("Logged out", "info")
     return redirect(url_for('home'))
 
-# ---------------- Dashboards
+# ---------------- Dash
 @app.route('/seller_dashboard')
 @login_required
 def seller_dashboard():
