5b4d14e7a10508dd71fe9727bb66a6db0c083811
chetan <156156147+chetan3622@users.noreply.github.com>
2026-07-19 18:37:52 +0530

    Fix comment formatting in app.py

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ def add_crop():
         return redirect(url_for('seller_dashboard'))
     return render_template("add_crops.html")
 
-# ---------------- View Crops for Buyer ----------------
+# ---------------- View Crops for Buyer 
 @app.route('/view_crops')
 @login_required
 def view_crops():
