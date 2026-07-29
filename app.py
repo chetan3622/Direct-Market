a0d4aa548be89bcae82067237878ee4995b814f4
chetan <156156147+chetan3622@users.noreply.github.com>
2026-07-30 00:03:50 +0530

    Fix comment formatting in app.py

--- app.py
+++ app.py
@@ -139,7 +139,7 @@ def show_interest(crop_id):
     flash("Interest sent to seller!", "success")
     return redirect(url_for('view_crops'))
 
-# ---------------- Seller Requests ----------------
+# ---------------- Seller Requests ----------
 @app.route('/seller_requests')
 @login_required
 def seller_requests():
