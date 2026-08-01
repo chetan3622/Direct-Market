9ffd655dfc281f48b6dace64d09cdce718b88161
chetan <156156147+chetan3622@users.noreply.github.com>
2026-08-01 10:37:22 +0530

    Remove commented-out code in app.py

--- app.py
+++ app.py
@@ -167,7 +167,6 @@ def update_request(interest_id, action):
     flash("Request updated!", "success")
     return redirect(url_for('seller_request'))
 
-# 
 @app.route('/notifications')
 @login_required
 def notifications():
